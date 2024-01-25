342e0acfd7a9fc1c5ae00d8aeb19b98a46ec5d8a
Sergey Maximov <dev9092336062@gmail.com>
2024-01-25 23:50:18 +0300

    start project

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./assets/styles/index.scss";
+import Page from "./components/Page";
+
+const App = () => {
+  return <Page />;
+};
+
+export default App;
---
+++ src/components/Page/index.tsx
@@ -0,0 +1,6 @@
+import styles from "./styles.module.scss";
+const Page = () => {
+  return <div className={styles.page}>todo</div>;
+};
+
+export default Page;
