d491783b43169f34a4102463367fe283f8706af8
Sergey Maximov <dev9092336062@gmail.com>
2024-01-27 23:19:34 +0300

    fix

--- src/api/links/getLinkIpApiCom.ts
+++ src/api/links/getLinkIpApiCom.ts
@@ -1,4 +1,4 @@
 export const getLinkIpApiCom = (ip?: string) =>
   ip
-    ? "//ip-api.com/json/${ip}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query"
+    ? `//ip-api.com/json/${ip}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query`
     : "//ip-api.com/json/?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query";
